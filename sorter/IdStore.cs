77817c4fce9adb14ee9ee8ccebacd1e94342eecb
IlyaPrusakou <beyourself@tut.by>
2019-05-30 02:13:17 +0300

    new

---
+++ sorter/IdStore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace sorter
+{
+     public class IdStore
+    {
+        public string Category { get; set; }
+        public List<string> Offer { get; set; }
+        public Dictionary<string, string> CategoryAndOffers { get; set; }
+        public IdStore()
+        {
+            CategoryAndOffers = new Dictionary<string, string>();
+        }
+
+        private string GetCategoryId (XmlReader rdr)
+        {
+            string result = null;
+            XDocument doc = XDocument.Load(rdr);
+            XElement rootYmlElement = doc.Root;
+            IEnumerable<XElement> CollectionOfCategories = rootYmlElement.Descendants("category");
+            foreach (XElement el in CollectionOfCategories)
+            {
+                CategoryAndOffers.Add(el.FirstAttribute.Value, null);
+            }
+            return result;
+        }
+        public void  GetOfferList(XmlReader rdr)
+        {
+            GetCategoryId(rdr);
+        }
+    }
+}
---
+++ sorter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace sorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\sorter\source\fullcatalog.yml";
+            IdStore store = new IdStore();
+            using (Stream fs = File.Open(path, FileMode.Open, FileAccess.ReadWrite))
+            {
+                XmlReader rdr = XmlReader.Create(fs);
+                store.GetOfferList(rdr);
+                foreach (string key in store.CategoryAndOffers.Keys)
+                {
+                    Console.WriteLine(key);
+                }
+            }
+            Console.WriteLine("end");
+            Console.ReadLine();
+        }
+    }
+}
